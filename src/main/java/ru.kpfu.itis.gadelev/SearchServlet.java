07f47b1fb9143a6a577c82a1037630a0f85c8581
Maxim Gadelev <maximgadelev@mail.ru>
2021-12-01 13:10:00 +0300

    "Last com"

--- src/main/java/ru.kpfu.itis.gadelev/SearchServlet.java
+++ src/main/java/ru.kpfu.itis.gadelev/SearchServlet.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
-SearchHelper searchHelper = new SearchHelper();
+    SearchHelper searchHelper = new SearchHelper();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("search.ftlh").forward(request, response);
@@ -21,10 +21,10 @@ SearchHelper searchHelper = new SearchHelper();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String bus = request.getParameter("bus");
         //ендпоинты не работали всю кр вслепую парсинг писал,по факту должно работать.
- String JSON = searchHelper.get("http://data.kzn.ru:8082/api/v0/dynamic_datasets/bus/"+bus+".json");
+        String JSON = searchHelper.get("http://data.kzn.ru:8082/api/v0/dynamic_datasets/bus/"+bus+".json");
         ObjectMapper mapper = new ObjectMapper();
         Map map = mapper.readValue(JSON, Map.class);
-        String normalString = new Gson().toJson(map);
+        String normalString = new Gson().toJson(map.get("data"));
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.getWriter().write(normalString);
